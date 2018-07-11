236335255e120e3b27667b8e4349cc985e30e2fa
Arg0s1080 <ivan.rincon76@gmail.com>
2018-07-12 00:34:22 +0200

    implement aes

--- mailform.py
+++ mailform.py
@@ -3,6 +3,7 @@ from PyQt5.QtWidgets import QDialog
 from configparser import ConfigParser
 from os.path import join, exists, dirname
 from os import getcwd, makedirs
+from scripts.aes import AESManaged
 import sys
 
 config_file = join(getcwd(), "mail.cfg")
@@ -14,6 +15,7 @@ class MailForm(QDialog):
         self.ui = Ui_MailDialog()
         self.ui.setupUi(self)
         self.config = ConfigParser()
+        self.aes = AESManaged("myPW")
         self.set_config()
         self.ui.lineEditFrom.textChanged.connect(self.line_edit_from_text_changed)
         self.ui.lineEditAlias.textChanged.connect(self.line_edit_alias_text_changed)
@@ -32,15 +34,15 @@ class MailForm(QDialog):
     def set_config(self):
         if exists(config_file):
             self.config.read(config_file)
-            self.ui.lineEditFrom.setText(self.config.get("Encrypted", "from"))
-            self.ui.lineEditAlias.setText(self.config.get("Encrypted", "alias"))
+            self.ui.lineEditFrom.setText(self.aes.decrypt(self.config.get("Encrypted", "from")))
+            self.ui.lineEditAlias.setText(self.aes.decrypt(self.config.get("Encrypted", "alias")))
             self.ui.lineEditServer.setText(self.config.get("General", "server"))
-            self.ui.lineEditPassword.setText(self.config.get("Encrypted", "password"))
+            self.ui.lineEditPassword.setText(self.aes.decrypt(self.config.get("Encrypted", "password")))
             self.ui.comboBoxEncrypt.setCurrentText(self.config.get("General", "encryption"))
             self.ui.spinBoxPort.setValue(self.config.getint("General", "port"))
-            self.ui.lineEditTo.setText(self.config.get("Encrypted", "to"))
-            self.ui.lineEditSubject.setText(self.config.get("Encrypted", "subject"))
-            self.ui.plainTextEditBody.setPlainText(self.config.get("Encrypted", "body"))
+            self.ui.lineEditTo.setText(self.aes.decrypt(self.config.get("Encrypted", "to")))
+            self.ui.lineEditSubject.setText(self.aes.decrypt(self.config.get("Encrypted", "subject")))
+            self.ui.plainTextEditBody.setPlainText(self.aes.decrypt(self.config.get("Encrypted", "body")))
         else:
             folder = dirname(config_file)
             if not exists(folder):
@@ -50,13 +52,14 @@ class MailForm(QDialog):
             self.config.set("General", "encryption", "TSL")
             self.config.set("General", "port", "587")
             self.config.add_section("Encrypted")
-            self.config.set("Encrypted", "from", "")
-            self.config.set("Encrypted", "alias", "")
-            self.config.set("Encrypted", "password", "")
-            self.config.set("Encrypted", "to", "")
-            self.config.set("Encrypted", "subject", "")
-            self.config.set("Encrypted", "body", "")
-            self.config.set("Encrypted", "attachment", "")
+            empty = self.aes.encrypt("")
+            self.config.set("Encrypted", "from", empty)
+            self.config.set("Encrypted", "alias", empty)
+            self.config.set("Encrypted", "password", empty)
+            self.config.set("Encrypted", "to", empty)
+            self.config.set("Encrypted", "subject", empty)
+            self.config.set("Encrypted", "body", empty)
+            self.config.set("Encrypted", "attachment", empty)
 
             with open(config_file, "w") as file:
                 self.config.write(file)
@@ -79,27 +82,27 @@ class MailForm(QDialog):
             print("Goodbye!")
 
     def line_edit_from_text_changed(self):
-        self.config.set("Encrypted", "from", self.ui.lineEditFrom.text())
+        self.config.set("Encrypted", "from", self.aes.encrypt(self.ui.lineEditFrom.text()))
 
-    def line_edit_alias_text_changed(self, event):
-        self.config.set("Encrypted", "alias", self.ui.lineEditAlias.text())
+    def line_edit_alias_text_changed(self):
+        self.config.set("Encrypted", "alias", self.aes.encrypt(self.ui.lineEditAlias.text()))
 
-    def line_edit_server_text_changed(self, event):
+    def line_edit_server_text_changed(self):
         self.config.set("General", "server", self.ui.lineEditServer.text())
 
-    def line_edit_password_text_changed(self, event):
-        self.config.set("Encrypted", "password", self.ui.lineEditPassword.text())
+    def line_edit_password_text_changed(self):
+        self.config.set("Encrypted", "password", self.aes.encrypt(self.ui.lineEditPassword.text()))
 
-    def line_edit_to_text_changed(self, event):
-        self.config.set("Encrypted", "to", self.ui.lineEditTo.text())
+    def line_edit_to_text_changed(self):
+        self.config.set("Encrypted", "to", self.aes.encrypt(self.ui.lineEditTo.text()))
 
-    def line_edit_subject_text_changed(self, event):
-        self.config.set("Encrypted", "subject", self.ui.lineEditSubject.text())
+    def line_edit_subject_text_changed(self):
+        self.config.set("Encrypted", "subject", self.aes.encrypt(self.ui.lineEditSubject.text()))
 
-    def plain_text_edit_body_text_changed(self, event):
-        self.config.set("Encrypted", "body", self.ui.plainTextEditBody.toPlainText())
+    def plain_text_edit_body_text_changed(self):
+        self.config.set("Encrypted", "body", self.aes.encrypt(self.ui.plainTextEditBody.toPlainText()))
 
-    def combobox_encrypt_current_index_changed(self, event):
+    def combobox_encrypt_current_index_changed(self):
         encrypt = self.ui.comboBoxEncrypt.currentText()
         if encrypt == "TSL":
             port = 587
