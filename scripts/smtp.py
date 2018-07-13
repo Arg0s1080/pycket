360160ee95cccedb18124557dccfac615854a0a4
Arg0s1080 <ivan.rincon76@gmail.com>
2018-07-13 20:11:49 +0200

    add _verify(address), debug_level, refactor

--- scripts/smtp.py
+++ scripts/smtp.py
@@ -6,17 +6,18 @@ from email.mime.base import MIMEBase
 from email import encoders
 from enum import Enum
 from os.path import isfile
+from re import match
 
 
 class Encrypt(Enum):
     ANY = 0,
-    STARTTSL = 1,
+    TSL = 1,
     SSL = 2
 
 
 class SendMail:
-    def __init__(self, smpt_server: str, port: int, from_address: str, password: str, to: str,
-                 from_alias="", subject="", body="", attachment=None, encrypt=Encrypt.STARTTSL):
+    def __init__(self, smpt_server: str, port: int, from_address: str, password: str, to: str, from_alias="",
+                 subject="", body="", attachment=None, encrypt=Encrypt.TSL, debug_level=False):
         self.smpt_server = smpt_server
         self.port = port
         self.from_ = from_address
@@ -27,11 +28,12 @@ class SendMail:
         self.body = body
         self.attachment = attachment
         self.encrypt = encrypt
+        self.debug_level = debug_level
 
     def send(self):
         msg = MIMEMultipart()
         msg["From"] = self.from_alias
-        msg["To"] = self.to
+        msg["To"] = self._verify(self.to)
         msg["Subject"] = self.subject
         msg.attach(MIMEText(self.body, "plain"))
 
@@ -51,26 +53,34 @@ class SendMail:
             server.ehlo()
         else:
             server = smtplib.SMTP(self.smpt_server, self.port)
-            if self.encrypt == Encrypt.STARTTSL:
+            if self.encrypt == Encrypt.TSL:
                 server.ehlo()
                 server.starttls()
             else:
                 server.ehlo_or_helo_if_needed()
-
+        server.set_debuglevel(self.debug_level)
         server.login(self.from_, self.password)
         mail = msg.as_string()
-        server.sendmail(self.from_, self.to, mail)
+        server.sendmail(self._verify(self.from_), self.to, mail)
         server.quit()
+
+    @staticmethod
+    def _verify(address: str):
+        if match("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$", address):
+            return address
+        else:
+            raise ValueError("Bad email address syntax", address)
 
 
 mail_ = SendMail(smpt_server="smtp.gmail.com", port=587,
                  from_address="ivan.rincon76@gmail.com",
-                 password="ofocabanisphhbbx",  # GMAIL App password
-                 to="ivan.rincon@ymail.com",
+                 password="",  # GMAIL App password
+                 to="xxxxxivan.rincon@ymxxxxxxail.com",
                  subject="prueba a yahoo 2",
                  body="únó dòŝ trêŝ",
                  attachment="/home/ivan/Escritorio/tabla.ods",
-                 encrypt=Encrypt.STARTTSL)
+                 encrypt=Encrypt.TSL,
+                 debug_level=True)
 mail_.send()
 
 # TSL 587
