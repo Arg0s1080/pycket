4f19fbeff78006087761fbc4393073a55f87eeeb
Arg0s1080 <ivan.rincon76@gmail.com>
2018-07-16 20:39:07 +0200

    add _verify(pw) and minor improvements

--- sendmail.py
+++ sendmail.py
@@ -1,10 +1,12 @@
 from configparser import ConfigParser
-from scripts.aes import AESManaged
+from scripts.aes import AESManaged, BadPasswordError
 from scripts.smtp import SMPTMail, Encrypt
 from os.path import join
 from os import getcwd
+from math import pi, e
 
 config_file = join(getcwd(), "mail.cfg")
+CONTROL = "%.15f%s%.14f%s" % (pi, "ck", e, "t")
 
 
 class SendMail:
@@ -12,6 +14,7 @@ class SendMail:
         self.aes = AESManaged(password)
         self.config = ConfigParser()
         self.config.read(mail_cfg)
+        self._verify(password)
         self.server = self.config.get("General", "server")
         self.port = self.config.getint("General", "port")
         self.from_ = self.aes.decrypt(self.config.get("Encrypted", "from"))
@@ -24,8 +27,10 @@ class SendMail:
         self.encrypt = Encrypt[self.config.get("General", "encryption")]
 
     def send(self):
-        self.attachment = self.attachment or None
         mail = SMPTMail(self.server, self.port, self.from_, self.pw, self.to, self.alias,
-                        self.subject, self.body, self.attachment, self.encrypt)
+                        self.subject, self.body, self.attachment or None, self.encrypt)
         mail.send()
 
+    def _verify(self, pw):
+        if CONTROL != self.aes.decrypt(self.config.get("Encrypted", "control")):
+            raise BadPasswordError(cause=pw)
