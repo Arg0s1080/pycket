b8071a5151024999fcf070d398bf8e6fa9975da8
Arg0s1080 <ivan.rincon76@gmail.com>
2018-07-26 20:30:07 +0200

    setting NotifyForm

--- forms/notify.py
+++ forms/notify.py
@@ -1,7 +1,7 @@
 from ui.notify_window import Ui_NotifyDialog
 from PyQt5.QtWidgets import QApplication, QDialog
 from PyQt5.QtGui import QCloseEvent, QFont
-from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import Qt, QTimer, QDateTime
 from configparser import ConfigParser
 from sys import argv, exit
 from os.path import exists, join
@@ -16,10 +16,21 @@ class NotifyForm(QDialog):
         self.ui = Ui_NotifyDialog()
         self.ui.setupUi(self)
         self.config = ConfigParser()
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer_tick)
+        self.delay = None
         self.set_config()
 
     def set_config(self):
         self.config.read(test_cfg(NOTIFY_CFG))
+        self.setWindowFlag(Qt.WindowStaysOnTopHint, self.config.getboolean("General", "on_top"))
+        self.delay = self.config.getint("General", "seconds")
+        self.config.getboolean("General", "auto_close") and self.timer.start(1000)
+        self.setWindowOpacity(self.config.getfloat("General", "opacity") / 100)
+        # TODO:
+        show_time = self.config.getboolean("General", "show_time")
+        # print(QDateTime.currentDateTime().toString("hh:mm:ss"))
+
         header_font = QFont(self.config.get("Header", "font"),
                             self.config.getint("Header", "size"),
                             italic=self.config.getboolean("Header", "italic"))
@@ -33,8 +44,10 @@ class NotifyForm(QDialog):
         self.ui.labelBody.setFont(body_font)
         self.ui.labelBody.setText(self.config.get("Body", "txt"))
 
-
-
+    def timer_tick(self):
+        if self.delay <= 0:
+            self.close()
+        self.delay -= 1
 
 
 if __name__ == '__main__':
