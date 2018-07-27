34a7542ff6c64b8d1abf54a32010d5c2df2946af
Arg0s1080 <ivan.rincon76@gmail.com>
2018-07-27 02:22:01 +0200

    timer show time

--- forms/notify.py
+++ forms/notify.py
@@ -10,27 +10,29 @@ from common.common import test_cfg
 # TODO: Move
 from provisional import NOTIFY_CFG
 
+
 class NotifyForm(QDialog):
     def __init__(self):
         super().__init__()
         self.ui = Ui_NotifyDialog()
         self.ui.setupUi(self)
         self.config = ConfigParser()
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.timer_tick)
+        self.timer_ac = QTimer()
+        self.timer_st = QTimer()
+        self.timer_ac.timeout.connect(self.timer_ac_tick)  # AutoClose
+        self.timer_st.timeout.connect(self.timer_st_tick)  # ShowTime
         self.delay = None
+        self.time_format = None
         self.set_config()
 
     def set_config(self):
         self.config.read(test_cfg(NOTIFY_CFG))
         self.setWindowFlag(Qt.WindowStaysOnTopHint, self.config.getboolean("General", "on_top"))
         self.delay = self.config.getint("General", "seconds")
-        self.config.getboolean("General", "auto_close") and self.timer.start(1000)
+        self.config.getboolean("General", "auto_close") and self.timer_ac.start(1000)
         self.setWindowOpacity(self.config.getfloat("General", "opacity") / 100)
-        # TODO:
-        show_time = self.config.getboolean("General", "show_time")
-        # print(QDateTime.currentDateTime().toString("hh:mm:ss"))
-
+        self.time_format = self.config.get("General", "time_format")
+        self.config.getboolean("General", "show_time") and self.timer_st.start(1000)
         header_font = QFont(self.config.get("Header", "font"),
                             self.config.getint("Header", "size"),
                             italic=self.config.getboolean("Header", "italic"))
@@ -44,10 +46,13 @@ class NotifyForm(QDialog):
         self.ui.labelBody.setFont(body_font)
         self.ui.labelBody.setText(self.config.get("Body", "txt"))
 
-    def timer_tick(self):
+    def timer_ac_tick(self):
         if self.delay <= 0:
             self.close()
         self.delay -= 1
+
+    def timer_st_tick(self):
+        self.ui.labelHeader.setText(QDateTime.currentDateTime().toString(self.time_format))
 
 
 if __name__ == '__main__':
