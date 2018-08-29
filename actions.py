f962d222989ad1e917f3ecba2e2a59b04b3fcced
Arg0s1080 <ivan.rincon76@gmail.com>
2018-08-29 20:26:34 +0200

    complete actions

--- actions.py
+++ actions.py
@@ -1,41 +1,59 @@
 from common.enums import Action
 from common.common import msg_dlg
-from provisional import MAIL_CFG, MAIN_CFG
+from paths import MAIL_CFG, MAIN_CFG
+from PyQt5.QtWidgets import QMessageBox
 
 
 def execute(action: Action, app_pw=None):
-    if action is Action.Notify:
-        from forms.notify import NotifyForm
-        NotifyForm().exec_()
-        print("Notify!!")
-    elif action is Action.Mail:
-        from scripts.sendmail import SendMail
-        from PyQt5.QtWidgets import QMessageBox
-        if app_pw is not None:
+    ok = None
+    try:
+        if action is Action.Notify:
+            from forms.notify import NotifyForm
+            ok = not NotifyForm().exec_()
+            print("Notify!!")
+        elif action is Action.Mail:
+            from scripts.sendmail import SendMail
+            if app_pw is not None:
+                try:
+                    mail = SendMail(app_pw, MAIL_CFG)
+                    mail.send()
+                    print("Sent Mail")
+                    ok = True
+                except Exception as ex:
+                    msg_dlg("Mail error", "An error occurred while sending mail", icon=QMessageBox.Critical,
+                            details="\n-".join(list(map(str, ex.args))))
+                    ok = False
+        else:
+            from configparser import ConfigParser
+            from subprocess import run, PIPE, Popen
+            from shlex import split
+            config = ConfigParser()
+            config.read(MAIN_CFG)
+            command = ""
+            if action is Action.Shutdown:
+                command = config.get("Commands", "shutdown")
+            elif action is Action.Reboot:
+                command = config.get("Commands", "reboot")
+            elif action is Action.CloseSession:
+                command = config.get("Commands", "close_session")
+            elif action is Action.Lock:
+                command = config.get("Commands", "lock_screen")
+            elif action is Action.Suspend:
+                command = config.get("Commands", "suspend")
+            elif action is Action.Hibernate:
+                command = config.get("Commands", "hibernate")
+            elif action is Action.Execute:
+                command = config.get("Commands", "execute")
             try:
-                mail = SendMail(app_pw, MAIL_CFG)
-                mail.send()
-                print("Sent Mail")
-            except Exception as ex:
-                msg_dlg("Mail error", "An error occurred while sending mail", icon=QMessageBox.Critical,
-                        details="\n-".join(list(map(str, ex.args))))
-    else:
-        from configparser import ConfigParser
-        config = ConfigParser()
-        config.read(MAIN_CFG)
-        command = ""
-        if action is Action.Shutdown:
-            command = config.get("Commands", "shutdown")
-        elif action is Action.Reboot:
-            command = config.get("Commands", "reboot")
-        elif action is Action.CloseSession:
-            command = config.get("Commands", "close_session")
-        elif action is Action.Lock:
-            command = config.get("Commands", "lock_screen")
-        elif action is Action.Suspend:
-            command = config.get("Commands", "suspend")
-        elif action is Action.Hibernate:
-            command = config.get("Commands", "hibernate")
-        elif action is Action.Execute:
-            command = config.get("Commands", "execute")
-
+                sp = Popen(split(command), stderr=PIPE)
+                _, std_err = sp.communicate()
+                if sp.returncode != 0:
+                    msg_dlg("Could not %s" % action.name, icon=QMessageBox.Critical,
+                            details="%s\nReturn Code: %s" % (std_err.decode(), sp.returncode))
+                ok = not sp.returncode
+            except FileNotFoundError as ex:
+                msg_dlg("Could not %s" % action.name, "Command not found", icon=QMessageBox.Critical,
+                        details="%s\nReturn Code: %s" % (ex.args[1], ex.args[0]))
+                ok = False
+    finally:
+        return ok
