e3c14e64ebc407f299920a6faecd5da42656a94b
Arg0s1080 <ivan.rincon76@gmail.com>
2018-08-30 20:52:04 +0200

    change paths

--- paths.py
+++ paths.py
@@ -1,17 +1,18 @@
-from os.path import join, expanduser
+from os.path import join, expanduser, realpath, dirname, pardir
 from os import getcwd
 from math import pi, e
 
 HOME = expanduser("~")
+PARENT = realpath(join(pardir, dirname(__file__)))
 CONFIG = join(HOME, ".config", "pycket")
-# MAIN_CFG = join(CONFIG, "config.cfg")
-# MAIL_CFG = join(CONFIG, "mail.cfg")
-# NOTIFY_CFG = join(CONFIG, "notify.cfg")
-
-# todo change
-MAIN_CFG = join(getcwd(), "cfg", "config.cfg")
-MAIL_CFG = join(getcwd(), "cfg", "mail.cfg")
-NOTIFY_CFG = join(getcwd(), "cfg", "notify.cfg")
+MAIN_CFG = join(CONFIG, "config.cfg")
+MAIL_CFG = join(CONFIG, "mail.cfg")
+NOTIFY_CFG = join(CONFIG, "notify.cfg")
+print("PATHS", PARENT)
+# Provisional
+# MAIN_CFG = join(getcwd(), "cfg", "config.cfg")
+# MAIL_CFG = join(getcwd(), "cfg", "mail.cfg")
+# NOTIFY_CFG = join(getcwd(), "cfg", "notify.cfg")
 
 # todo delete
 GMAIL_APP_PW = "geuipfkvdhhxtmxi"
